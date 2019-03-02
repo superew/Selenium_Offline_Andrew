220e0ca3921641e4dd317b61631540d0222b2ec7
dunghvnguyen <dunghvnguyen@2S9ZQN2.kms.com.vn>
2019-03-02 14:46:08 +0700

    Commit 4th time

--- src/WebDriver_API/Exercise1.java
+++ src/WebDriver_API/Exercise1.java
@@ -1,7 +1,6 @@
 package WebDriver_API;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 import static org.testng.Assert.assertEquals;
 
@@ -9,6 +8,7 @@ import java.util.Random;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -21,73 +21,73 @@ public class Exercise1 {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 	}
-	@Test
-	public void TC_01_loginEmpty() {
-		//Open link
-		driver.get("http://live.guru99.com/");
-		
-		//Click on Account link
-		driver.findElement(By.xpath("//a[contains(@class,'skip-account')]")).click();
-		
-		//Click on My Account to navigate to Login page
-		driver.findElement(By.xpath("//div[@id='header-account']//a[contains(@title,'My Account')]")).click();
-		
-		//Click on button Login
-		driver.findElement(By.xpath("//button[@title='Login']")).click();
-		
-		//Get error texts
-		String errorText = driver.findElement(By.xpath("//div[@class='validation-advice']")).getText();
-		assertEquals(errorText, "This is a required field.");
-		
-	}
+//	@Test
+//	public void TC_01_loginEmpty() {
+//		//Open link
+//		driver.get("http://live.guru99.com/");
+//		
+//		//Click on Account link
+//		driver.findElement(By.xpath("//a[contains(@class,'skip-account')]")).click();
+//		
+//		//Click on My Account to navigate to Login page
+//		driver.findElement(By.xpath("//div[@id='header-account']//a[contains(@title,'My Account')]")).click();
+//		
+//		//Click on button Login
+//		driver.findElement(By.xpath("//button[@title='Login']")).click();
+//		
+//		//Get error texts
+//		String errorText = driver.findElement(By.xpath("//div[@class='validation-advice']")).getText();
+//		assertEquals(errorText, "This is a required field.");
+//		
+//	}
+//	
+//	@Test
+//	public void TC_02_loginInvalidEmail() {
+//		//Open link
+//		driver.get("http://live.guru99.com/");
+//				
+//		//Click on Account link
+//		driver.findElement(By.xpath("//a[contains(@class,'skip-account')]")).click();
+//				
+//		//Click on My Account to navigate to Login page
+//		driver.findElement(By.xpath("//div[@id='header-account']//a[contains(@title,'My Account')]")).click();
+//		
+//		//Input Invalid Email
+//		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("123434234@12312.123123");
+//		
+//		//Click on button Login
+//		driver.findElement(By.xpath("//button[@title='Login']")).click();
+//		
+//		String errorText = driver.findElement(By.xpath("//div[contains(@id,'validate-email')]")).getText();
+//		assertEquals(errorText, "Please enter a valid email address. For example johndoe@domain.com.");
+//	}
+//	
+//	@Test
+//	public void TC_03_loginIncorrectPass() {
+//		//Open link
+//		driver.get("http://live.guru99.com/");
+//						
+//		//Click on Account link
+//		driver.findElement(By.xpath("//a[contains(@class,'skip-account')]")).click();
+//						
+//		//Click on My Account to navigate to Login page
+//		driver.findElement(By.xpath("//div[@id='header-account']//a[contains(@title,'My Account')]")).click();
+//		
+//		//Input Email
+//		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("automation@gmail.com");
+//		
+//		//Input incorrect Pass
+//		driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("123");
+//		
+//		//Click on button Login
+//		driver.findElement(By.xpath("//button[@title='Login']")).click();
+//		
+//		//Verify text
+//		String errorText = driver.findElement(By.xpath("//div[contains(@id,'validate-password')]")).getText();
+//		assertEquals("Please enter 6 or more characters without leading or trailing spaces.", errorText);
+//	}
 	
-	@Test
-	public void TC_02_loginInvalidEmail() {
-		//Open link
-		driver.get("http://live.guru99.com/");
-				
-		//Click on Account link
-		driver.findElement(By.xpath("//a[contains(@class,'skip-account')]")).click();
-				
-		//Click on My Account to navigate to Login page
-		driver.findElement(By.xpath("//div[@id='header-account']//a[contains(@title,'My Account')]")).click();
-		
-		//Input Invalid Email
-		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("123434234@12312.123123");
-		
-		//Click on button Login
-		driver.findElement(By.xpath("//button[@title='Login']")).click();
-		
-		String errorText = driver.findElement(By.xpath("//div[contains(@id,'validate-email')]")).getText();
-		assertEquals(errorText, "Please enter a valid email address. For example johndoe@domain.com.");
-	}
 	
-	@Test
-	public void TC_03_loginIncorrectPass() {
-		//Open link
-		driver.get("http://live.guru99.com/");
-						
-		//Click on Account link
-		driver.findElement(By.xpath("//a[contains(@class,'skip-account')]")).click();
-						
-		//Click on My Account to navigate to Login page
-		driver.findElement(By.xpath("//div[@id='header-account']//a[contains(@title,'My Account')]")).click();
-		
-		//Input Email
-		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("automation@gmail.com");
-		
-		//Input incorrect Pass
-		driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("123");
-		
-		//Click on button Login
-		driver.findElement(By.xpath("//button[@title='Login']")).click();
-		
-		//Verify text
-		String errorText = driver.findElement(By.xpath("//div[contains(@id,'validate-password')]")).getText();
-		assertEquals("Please enter 6 or more characters without leading or trailing spaces.", errorText);
-	}
-	
-	@Test
 	//Create random email
 	public static String randomEmail() {
 		Random rand = new Random();
@@ -95,9 +95,9 @@ public class Exercise1 {
 		return String.valueOf(number);
 	}
 	
-	String email = "random" + randomEmail() + "@gmail.com";
-	
+	@Test
 	public void TC_04_createNewAccount() {
+		String email = "random" + randomEmail() + "@gmail.com";
 		//Open link
 		driver.get("http://live.guru99.com/");
 								
@@ -130,7 +130,7 @@ public class Exercise1 {
 		
 		//Verify My Dashboard page
 		String mydashboard = driver.findElement(By.xpath("//div[@class='page-title']/h1")).getText();
-		assertEquals("My Dashboard", mydashboard);
+		assertEquals("MY DASHBOARD", mydashboard);
 		
 		//Verify text
 		String errorText2 = driver.findElement(By.xpath("//ul[@class='messages']//span")).getText();
@@ -161,11 +161,11 @@ public class Exercise1 {
 		
 		//Verify current URL
 		String currentURL = driver.getCurrentUrl();
-		assertEquals("http://live.guru99.com/index.php/customer/account/index/", currentURL);
+		Assert.assertEquals("http://live.guru99.com/index.php/customer/account/index/", currentURL);
 	}
 	@AfterClass
 	public void afterClass() {
-		driver.quit();
+//		driver.quit();
 	}
 	
 	
